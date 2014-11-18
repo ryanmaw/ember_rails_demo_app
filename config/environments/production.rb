a4f80b3970f7bd1f70a30dc229da3ee484c95b9e
ryanmaw <ryan@ryanmaw.com>
2014-11-18 10:44:52 -0700

    Ran Ember installer; ember.variant set

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,13 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # :production uses minified and has no logging
+  config.ember.variant = :production
+
+
+
+
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
