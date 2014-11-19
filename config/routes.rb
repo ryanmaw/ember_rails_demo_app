461e2fd4b0be318a1de532d84b2bbe19e47cc79e
ryanmaw <ryan@ryanmaw.com>
2014-11-18 17:54:24 -0700

    API Controller Routes Added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   root to: 'home#index'
 
+  namespace :api do
+    namespace :v1 do
+      resources :members
+    end
+  end
+  
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
